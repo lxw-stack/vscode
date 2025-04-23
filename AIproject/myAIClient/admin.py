3b4836f7094d3d194f89216f1e2e514e68acc56d
lt <123>
2025-04-24 00:05:46 +0800

    vue

--- AIproject/myAIClient/admin.py
+++ AIproject/myAIClient/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from myAIClient.models import NeedFiles, Contents
 
-# Register your models here.
+@admin.register(NeedFiles)
+class NeedFilesAdmin(admin.ModelAdmin):
+    list_display = ('project', 'requirement_doc', 'version')
+    search_fields = ('project', 'requirement_doc')
+
+@admin.register(Contents)
+class ContentsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'needfile')
+    list_filter = ('needfile',)
+    raw_id_fields = ('needfile',)
--- AIproject/myAIClient/models.py
+++ AIproject/myAIClient/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class NeedFiles(models.Model):
+    project = models.CharField(max_length=255, verbose_name='所属项目')
+    requirement_doc = models.CharField(max_length=255, verbose_name='需求文档名称')
+    version = models.CharField(max_length=50, verbose_name='版本号')
+    
+    class Meta:
+        db_table = 'needfiles'  # 指定使用已存在的表名
+        verbose_name = '需求文档表'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return f"{self.project}-{self.requirement_doc}"
+
+class Contents(models.Model):
+    needfile = models.ForeignKey(NeedFiles, on_delete=models.CASCADE, db_column='needfile_id', verbose_name='关联需求文档')
+    title = models.CharField(max_length=255, verbose_name='分段标题')
+    content = models.TextField(verbose_name='分段内容', blank=True, null=True)
+    
+    class Meta:
+        db_table = 'contents'  # 指定使用已存在的表名
+        verbose_name = '需求文档内容分段表'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
